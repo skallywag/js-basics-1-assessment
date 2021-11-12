d2b53521b8cdec8fcd637bf116b34f4cec6a9ae5
Samuel Lee <lee.sam1492@gmail.com>
2021-11-12 11:48:36 -0700

    completed password.js

---
+++ password.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("Please validate your password!", function(input){
+//-----It seems to be working but i feel like i still did this wrong?
+
+    let validPass = input.length
+
+        if(validPass <= 10){
+            console.log('Success!');
+        }else{
+            console.log('Password failed');
+        }
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
